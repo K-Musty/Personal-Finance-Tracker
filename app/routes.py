011060bf9c417e9990f50eae0eecae3d3f2ea91c
K-Musty <kmustapha9564@gmail.com>
2025-05-07 23:37:06 +0100

    PFT: Adding login feature

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 
 bp = Blueprint('main', __name__)
 
@@ -6,10 +6,17 @@ bp = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+bp.route('/dashboard')
+def user_dashboard():
+    return render_template('dashboard.html')
+
 @bp.route('/signup')
 def signup():
     return render_template("signup.html")
 
-@bp.route('/login')
+@bp.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        flash("Login Successful !!!")
+        return redirect(url_for('main.user_dashboard'))
     return render_template("login.html")
