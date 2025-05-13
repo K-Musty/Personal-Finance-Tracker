26774f1264a50b4b25f2abdccfd75e32cea11f38
K-Musty <kmustapha9564@gmail.com>
2025-05-13 23:06:00 +0100

    PFT: Handling routes, making 'User' and 'Transaction' Tables

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.COlumn(db.String(100), nullable=False, unique=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
     transactions = db.relationship('Transaction', backref='user', lazy=True)
@@ -12,4 +12,4 @@ class Transaction(db.Model):
     amount = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200))
-    user_id = db.Column(db.Integer, db.Foreignkey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
