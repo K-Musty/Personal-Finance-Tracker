1b6c0a3eebf940464bebbd68ea65650acbe2f624
K-Musty <kmustapha9564@gmail.com>
2025-05-14 23:50:17 +0100

    PFT: Getting started with user authentication

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
